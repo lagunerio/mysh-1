1a812a9634a1fbf000c98e9efcc96edafe881cd7
lagunerio <lagunerio@gmail.com>
2017-11-12 21:06:48 +0900

    error handling; define MAX_LIND

--- src/commands.c
+++ src/commands.c
@@ -6,6 +6,8 @@
 #include "commands.h"
 #include "built_in.h"
 
+#define MAX_LINE 256
+
 static struct built_in_command built_in_commands[] = {
   { "cd", do_cd, validate_cd_argv },
   { "pwd", do_pwd, validate_pwd_argv },
