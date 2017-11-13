f709aa90a017d31b62ade8da2eb53ed139c32014
lagunerio <lagunerio@gmail.com>
2017-11-14 01:58:05 +0900

    signal handling
    10th

--- src/main.c
+++ src/main.c
@@ -11,10 +11,10 @@
 
 
 //structure to handle signals
-int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
+int sigaction(int signum, const struct sigaction *act);
 struct sigaction
 {
-  void(*sa_handler)int
+  //void(*sa_handler)int
   sigset_t sa_mask;
   int sa_flags;
 }
@@ -28,15 +28,15 @@ int main()
 
     int i=0;
     struct sigaction act;
-    act.sa_handler = my_signal;
+    //act.sa_handler = my_signal;
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
 
-    if(sigaction(SIGINT, &act, 0) == SIG_ERR){
+    if(sigaction(SIGINT, &act) == SIG_ERR){
       catch_sigint(1);
       continue;
     }
-    if(sigaction(SIGTSTP, &act, 0) == SIG_ERR){
+    if(sigaction(SIGTSTP, &act) == SIG_ERR){
       catch_sigtstp(2);
       continue;
     }
