9ed774880d1944839576aa389527286053aa008a
lagunerio <lagunerio@gmail.com>
2017-11-14 01:46:30 +0900

    signal handling
    9th try

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "signal.h"
 #include "commands.h"
 #include "built_in.h"
 #include "utils.h"
 
-/*
+
 //structure to handle signals
 int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact);
 
@@ -16,12 +18,8 @@ struct sigaction
   void(*sa_handler)int
   sigset_t sa_mask;
   int sa_flags;
-
-
-
 }
-*/
-//struct sigaction act;
+struct sigaction act;
 
 int main()
 {
@@ -29,14 +27,22 @@ int main()
 
   while (1) {
 
-    signal(SIGINT, catch_sigint(SIGINT));
-    signal(SIGTSTP, catch_sigtstp(SIGTSTP));
-    //signal(SIGSTP, catch_sigtstp());
+    int i=0;
+    struct sigaction act;
+    act.sa_handler = my_signal;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+
+    if(sigaction(SIGINT, &act, 0) == SIG_ERR){
+      catch_sigint(1);
+      continue;
+    }
+    if(sigaction(SIGTSPT, &act, 0) == SIG_ERR){
+      catch_sigtstp(2);
+      continue;
+    }
     fgets(buf, 8096, stdin);
 
-    //sigemptyset(&act.sa_mask);
-
-    //fgets(buf, 8096, stdin);
     struct single_command commands[512];
     int n_commands = 0;
     mysh_parse_command(buf, &n_commands, &commands);
