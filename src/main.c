b90125a1b5bff7b8103f3a2c0aa915e335cf9189
lagunerio <lagunerio@gmail.com>
2017-11-14 17:25:58 +0900

    add functions, handle errors

--- src/main.c
+++ src/main.c
@@ -1,60 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <signal.h>
 
-#include "signal.h"
 #include "commands.h"
 #include "built_in.h"
 #include "utils.h"
+#include "signal_handlers.h"
 
-/*
-//structure to handle signals
-int sigaction(int signum, const struct sigaction *act);
-struct sigaction
-{
-  //void(*sa_handler)int
-  sigset_t sa_mask;
-  int sa_flags;
-}
-
-struct sigaction act;
-*/
 int main()
 {
   char buf[8096];
-  struct sigaction act_int;
-  //struct sigaction act_tstp;
-  act_int.sa_handler = catch_sigint();
-  //act_tstp.sa_handler = catch_sigtstp();
+
+  // First check for interups (We will ignor both C-c and C-z
+  signal(SIGTSTP,catch_sigtstp);      //for Ctrl+Z
+  signal(SIGINT,catch_sigint);        //for Ctrl+C
 
 
   while (1) {
-
-    sigemptyset(&act_int.sa_mask);
-    //sigemptyset(&act_tstp.sa_mask);
-    act_int.sa_flags = 0;
-    //act_tstp.sa_flags = 0;
-/*
-    if(sigaction(SIGINT, &act) == SIG_ERR){
-      catch_sigint(1);
-      continue;
-    }
-    if(sigaction(SIGTSTP, &act) == SIG_ERR){
-      catch_sigtstp(2);
-      continue;
-    }
-*/
-    if(sigaction(SIGINT, &act_int, 0)== SIG_ERR){
-      printf("sigaction() error; act_int\n");
-      exit(1);
-    }
-    //else if(sigaction(SIGTSTP, &act_tstp, 0)== SIG_ERR){
-    //  printf("sigaciton() error; act_tstp\n");
-    //  exit(1);
-    //}
-
     fgets(buf, 8096, stdin);
 
     struct single_command commands[512];
--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -11,3 +11,26 @@ void catch_sigtstp(int signalNo)
   // TODO: File this!
   printf("sigtstp\n");
 }
+#include <stdio.h>
+#include <signal.h>
+#include "signal_handlers.h"
+
+void catch_sigint(int signalNo)
+{
+  // Implemetation of ctrl-C
+  signal(SIGINT, SIG_IGN);
+
+  // printf ("This is control-C \n");
+
+  fflush(stdout);
+}
+
+void catch_sigtstp(int signalNo)
+{
+  // Implementatio of ctrol-Z
+   signal(SIGTSTP, SIG_IGN);
+
+   //  printf ("This is control-Z \n");
+
+   fflush(stdout);
+}
